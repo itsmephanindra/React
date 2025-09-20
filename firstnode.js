00f376e0b20bcfd1b084f65cf7feefd0ff5e9ef4
itsmephanindra <phanindrabulusu@gmail.com>
2025-09-20 18:09:58 +0530

    React learning commit

---
+++ firstnode.js
@@ -0,0 +1,26 @@
+console.log("Hello all");
+
+var userName="Phanindra";
+var emailId="shinning.ruby@gmail.com";
+var noOfCharsInName;
+
+
+
+console.log(userName);
+console.log(emailId);
+
+noOfCharsInName=userName.length;
+
+console.log("Length of name is " + noOfCharsInName);
+
+noOfCharsInName=emailId.length;
+console.log("Length of Email id is "+ noOfCharsInName);
+
+console.log("Phanindra"=="Phanindra");
+
+console.log(userName.toUpperCase());
+
+var tehnologies="Html,csss,Node,React,Python,oracle";
+
+console.log(tehnologies);
+console.log(tehnologies.split(','));
---
+++ secondcode.js
@@ -0,0 +1,20 @@
+var priceOriginal=20000
+var discountPrice=1000;
+
+var finalPrice;
+
+finalPrice=priceOriginal-discountPrice;
+
+console.log("Price original " + priceOriginal);
+
+console.log("disCount " + discountPrice);
+
+console.log("Final price " + finalPrice);
+
+var number=1;
+number=number+1;
+console.log(number);
+number+=1;
+console.log(number);
+number++;
+console.log(number);
