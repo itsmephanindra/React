b64a5da43b376a3c7f2419875d89297a324563cd
itsmephanindra <phanindra.bulusu@gmail.com>
2025-09-21 15:37:17 +0530

    Commit on 21-sep

--- jsonCode.js
+++ jsonCode.js
@@ -9,3 +9,6 @@ var userDetails={
 console.log(userName);
 console.log(userDetails.name);
 console.log(userDetails.age);
+console.log(userDetails.name.toUpperCase());
+console.log(userDetails.name.length);
+console.log(userDetails.name.toLowerCase());
