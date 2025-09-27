1d16ee6dd619ceb624598a0d3928d000178c8712
itsmephanindra <phanindra.bulusu@gmail.com>
2025-09-27 15:30:29 +0530

    forLoop code

---
+++ forLoop.js
@@ -0,0 +1,8 @@
+var ecommerceProducts = [
+    {        "id": 1,           "name": "Laptop",        "price": 999.99,        "category": "Electronics"    },  
+    {        "id": 2,        "name": "Smartphone",        "price": 699.99,        "category": "Electronics"}];
+
+
+    for (i=0; i<ecommerceProducts.length; i++) {
+        console.log("E-commerce Products:", ecommerceProducts[i].id, ecommerceProducts[i].name, ecommerceProducts[i].price, ecommerceProducts[i].category);
+    };
