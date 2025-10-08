7ae1bf3908f40b4ec98d3a4d668e6e90ddf7a5bc
itsmephanindra <phanindra.bulusu@gmail.com>
2025-10-09 00:14:07 +0530

    New project

---
+++ react-project/src/Login.js
@@ -0,0 +1,8 @@
+function Login(){
+
+    return(
+       <h3> This is Login Page</h3>
+    )
+}
+
+export default Login;
---
+++ react-project/src/Signup.js
@@ -0,0 +1,9 @@
+function Signup(){
+
+    return(
+        <h4>This is signup page</h4>
+    )
+    
+}
+
+export default Signup;
